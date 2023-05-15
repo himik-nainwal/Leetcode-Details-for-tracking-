2f16b6041cf256cf2882749750df55208e1c6f34
Himik Nainwal <himithnainwal@gmail.com>
2023-05-15 19:50:59 +0530

    DOne with all

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import * as XLSX from 'xlsx';
 
-function App() {
+function ExcelParser() {
+  const [userData, setUserData] = useState([]);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const data = e.target.result;
+      const workbook = XLSX.read(data, { type: 'binary' });
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const usernameColumn = 'username'; // Replace with your desired column name
+
+      const rows = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      const headerRow = rows[0];
+      const usernameColumnIndex = headerRow.findIndex((cell) => cell === usernameColumn);
+
+      if (usernameColumnIndex !== -1) {
+        const userData = rows.slice(1).map((row) => row[usernameColumnIndex]);
+        setUserData(userData);
+      } else {
+        console.log(`Column "${usernameColumn}" not found`);
+      }
+    };
+
+    reader.readAsBinaryString(file);
+  };
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
+      <ul>
+        {userData.map((username, index) => (
+          <li key={index}>{username}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
-export default App;
+export default ExcelParser;
--- python-script/main.py
+++ python-script/main.py
@@ -1,13 +1,14 @@
+import time
 import pandas as pd
 import requests
 
-# Read the input excel file
+# Read the input Excel file into a pandas DataFrame
 input_df = pd.read_excel('input.xlsx')
 
-# Define the GraphQL endpoint URL
+# API endpoint URL
 url = 'https://leetcode.com/graphql'
 
-# Define the GraphQL query
+# GraphQL query to fetch user information
 query = """
 query ($username: String!) {
   matchedUser(username: $username) {
@@ -15,10 +16,19 @@ query ($username: String!) {
     userCalendar {
       activeYears
     }
+    githubUrl
+    contributions {
+      points
+      questionCount
+      testcaseCount
+    }
     profile {
+      skillTags
       ranking
       aboutMe
       realName
+      reputation
+      countryName
     }
     submitStatsGlobal {
       acSubmissionNum {
@@ -31,24 +41,70 @@ query ($username: String!) {
 }
 """
 
-# Define a function to execute GraphQL queries
+# Function to execute a GraphQL query using the requests library
 def execute_query(query, variables):
     response = requests.post(url, json={'query': query, 'variables': variables})
     return response.json()['data']
 
-# Loop over each row of the input dataframe and fetch data for each user
 results = []
+counter = 0
+
+# Iterate over each row in the input DataFrame
 for i, row in input_df.iterrows():
     username = row['Username']
     variables = {'username': username}
+    
+    # Execute the GraphQL query for the current username
     result = execute_query(query, variables)
+    
     if result.get('matchedUser'):
-        results.append(result['matchedUser'])
-    else:
-        results.append({'username': username, 'userCalendar': {'activeYears': None}, 'profile': {'ranking': None, 'aboutMe': None, 'realName': None}, 'submitStatsGlobal': {'acSubmissionNum': None}})
+        # Process the user data if a match is found
+        for x in result["matchedUser"]["submitStatsGlobal"]["acSubmissionNum"]:
+            # Add difficulty count in the format "count/submissions"
+            result["matchedUser"][x["difficulty"]] = str(x["count"]) + "/" + str(x["submissions"])
 
-# Convert the list of results to a dataframe
+        # Extract and reorganize relevant user information
+        result["matchedUser"]["ranking"] = result["matchedUser"]["profile"]["ranking"]
+        result["matchedUser"]["country"] = result["matchedUser"]["profile"]["countryName"]
+        result["matchedUser"]["reputation"] = result["matchedUser"]["profile"]["reputation"]
+        result["matchedUser"]["contribution-points"] = result["matchedUser"]["contributions"]["points"]
+        result["matchedUser"]["githubUrl"] = result["matchedUser"]["githubUrl"]
+        # POP is used to drop some objects which are not in need
+        # make sure to extract from that
+        # object before. And look at that query adding and 
+        # deleting/droping can be done.
+        result["matchedUser"].pop("profile")
+        result["matchedUser"].pop("contributions")
+        result["matchedUser"].pop("submitStatsGlobal")
+        result["matchedUser"].pop("userCalendar")
+
+        # Create a dictionary to store the combined data
+        result_dict = {}
+        for col in input_df.columns:
+            if col != 'Username':
+                result_dict[col] = row[col]
+        for col in result["matchedUser"]:
+            result_dict[col] = result["matchedUser"][col]
+        
+        # Append the combined data to the results list
+        results.append(result_dict)
+    else:
+        # If no match is found, create a dictionary with the username only
+        result_dict = {'username': username}
+        for col in input_df.columns:
+            if col != 'Username':
+                result_dict[col] = row[col]
+        results.append(result_dict)
+
+    counter += 1
+    
+    # Delay execution every 150 records to avoid IP blocking
+    if counter % 150 == 0:
+        print(f"Processed {counter} records. Waiting for 10 seconds...To avoid IP block")
+        time.sleep(10)
+
+# Convert the results list to a pandas DataFrame
 output_df = pd.json_normalize(results)
 
-# Save the output dataframe to a new Excel file
+# Write the output DataFrame to an Excel file
 output_df.to_excel('updated-sheet.xlsx', index=False)
